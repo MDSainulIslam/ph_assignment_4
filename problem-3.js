6cdb3338d3a18024bda46889b0cdf7048289383e
lisan <mdsainulislamlisan@gmail.com>
2025-08-13 20:08:35 +0600

    problem-3 succesfully solved

---
+++ problem-3.js
@@ -0,0 +1,18 @@
+function  bestTeam( player1, player2 ) {
+    if(typeof player1 !== "object" || typeof player2 !== "object"){
+        return "Invalid"
+    }
+
+    let player1Marks = player1.foul + player1.cardY + player1.cardR;
+    let player2Marks = player2.foul + player2.cardY + player2.cardR;
+
+    if(player1Marks === player2Marks){
+        return "Tie";
+    }
+    else if(player1Marks < player2Marks){
+        return player1.name;
+    }
+    else{
+        return player2.name
+    }
+}
