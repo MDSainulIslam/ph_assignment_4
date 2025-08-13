275b6cab9e97267a713e11e97a113be0ff217306
lisan <mdsainulislamlisan@gmail.com>
2025-08-13 23:00:16 +0600

    problem-5 successfully solved

--- solution.js
+++ solution.js
@@ -84,3 +84,42 @@ function  isSame(arr1 , arr2 ) {
 
 
 
+function  resultReport( marks ) {
+    let passCount = 0;
+    let failCount = 0;
+    let totalMarks =0;
+    
+    if(Array.isArray(marks) !== true){
+        return "Invalid";
+    }
+
+    if(marks.length === 0){
+        return {
+            finalScore: 0,
+            pass: 0,
+            fail: 0 };
+    }
+
+
+    for(let mark of marks){
+
+        totalMarks = totalMarks + mark;
+
+        if(mark >= 40){
+            passCount++ ;
+        }
+        else{
+            failCount++ ;
+        }  
+    }
+
+    let averageMarks = Math.round(totalMarks/marks.length);
+
+    let finalResult = {
+        finalScore : averageMarks,
+        pass : passCount,
+        fail : failCount
+    };
+    return finalResult;
+}
+
